3352c68be1457f6b04e510002c48983c27a4f30d
buck84 <buck84@126.com>
2019-07-27 08:18:58 +0800

    init

---
+++ src/0001HelloDot/0002HelloDot.cpp
@@ -0,0 +1,87 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+//  Triangles.cpp
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#include "vgl.h"
+#include "LoadShaders.h"
+
+enum Buffer_IDs { ArrayBuffer, NumBuffers };
+GLuint  Buffers[NumBuffers];
+
+//----------------------------------------------------------------------------
+//
+// init
+//
+
+
+void
+init(void)
+{
+	GLfloat Vertices[3] = { 0.0f, 0.0f, 0.0f };
+
+	glGenBuffers(1, Buffers);
+	glBindBuffer(GL_ARRAY_BUFFER, Buffers[ArrayBuffer]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+}
+
+//----------------------------------------------------------------------------
+//
+// display
+//
+
+void
+display(void)
+{
+	static const float black[] = { 0.0f, 0.0f, 0.0f, 0.0f };
+
+	glClearBufferfv(GL_COLOR, 0, black);
+
+
+	//glEnableVertexAttribArray(0);
+	//glBindBuffer(GL_ARRAY_BUFFER, Buffers[ArrayBuffer]);
+	//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+	glDrawArrays(GL_POINTS, 0, 1);
+
+	//glDisableVertexAttribArray(0);
+}
+
+//----------------------------------------------------------------------------
+//
+// main
+//
+
+#ifdef _WIN32
+int CALLBACK WinMain(
+	_In_ HINSTANCE hInstance,
+	_In_ HINSTANCE hPrevInstance,
+	_In_ LPSTR     lpCmdLine,
+	_In_ int       nCmdShow
+)
+#else
+int
+main(int argc, char** argv)
+#endif
+{
+	glfwInit();
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Triangles", NULL, NULL);
+
+	glfwMakeContextCurrent(window);
+	gl3wInit();
+
+	init();
+
+	while (!glfwWindowShouldClose(window))
+	{
+		display();
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+
+	glfwTerminate();
+}
